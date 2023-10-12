ddeac1304aa9268fde21bee967a96a81712ced77
estiven2111 <estiven.a.martinez@gmail.com>
2023-10-11 21:04:55 -0500

    Estiven process.env.PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 require("./src/db")
-const PORT = 3200
+const PORT = process.env.PORT || 3200
 
 app.listen(PORT, () =>{
     console.log(`escuchando por el puerto ${PORT}`)
